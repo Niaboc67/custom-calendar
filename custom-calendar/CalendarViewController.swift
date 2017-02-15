bf4e29717bb170cdc1d3882d9d6b8e692eee31af
jen2 <sipila.jen@gmail.com>
2017-02-14 16:14:11 -0800

    initial set up

---
+++ custom-calendar/CalendarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CalendarViewController.swift
+//  custom-calendar
+//
+//  Created by Jennifer A Sipila on 2/14/17.
+//  Copyright © 2017 Jennifer A Sipila. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var calendarCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
