9a59188e5087921a210e40b6f5ca6d4e7cca49c1
jen2 <sipila.jen@gmail.com>
2017-02-16 11:00:03 -0500

    Setting up storyboard

--- custom-calendar/CalendarViewController.swift
+++ custom-calendar/CalendarViewController.swift
@@ -14,9 +14,54 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    // MARK: - UICollectionViewDatasource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 35
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "dayCell", for: indexPath)
+        
+        //Format cell
+        
+        return cell
+    }
+    
+
+    
+
+    
+    // MARK: - UICollectionViewDelegate / Layout
+    
+    func collectionView
+    
+    - (CGSize)collectionView:(UICollectionView *)collectionView
+    layout:(UICollectionViewLayout*)collectionViewLayout
+    sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(HSCardWidth, HSCardHeight);
+    }
+    
+    - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(nonnull UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return self.sectionInsets;
+    }
+    
+    - (CGFloat) collectionView:(UICollectionView *)collectionView layout:(nonnull UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return HSInterCardVerticalSpacing;
+    }
+    
+    - (CGFloat) collectionView:(UICollectionView *) collectionView
+    layout:(UICollectionViewLayout *) collectionViewLayout
+    minimumInteritemSpacingForSectionAtIndex:(NSInteger) section {
+    return HSInterCardHorizontalSpacing;
+    }
+
     
 
     /*
